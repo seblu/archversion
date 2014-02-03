ff3be75dc4ba353201ebe02193a99c2d415ee264
Sébastien Luttringer <seblu@seblu.net>
2014-02-03 13:26:50 +0100

    Fix use of regex_exclude string in matching

--- src/lib/archversion/version.py
+++ src/lib/archversion/version.py
@@ -109,7 +109,7 @@ class VersionController(object):
                 regex_exclude = value.get("regex_exclude", ".*(rc|beta|alpha).*")
                 if regex_exclude != "":
                     logging.debug("Exclusion regex: %s" % regex_exclude)
-                    v -= set(filter(lambda x: "rc" in x, v))
+                    v -= set(filter(lambda x: re.match(regex_exclude, x), v))
                     logging.debug("Found versions after exclusion: %s" % v)
                 # latest version is the highest
                 v = max(v, key=VersionKey)
